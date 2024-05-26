4600fa8073ca251bc7182f6c4c9b77eed292bd44
GarnierLenny <61317098+GarnierLenny@users.noreply.github.com>
2024-05-26 12:11:37 +0900

    auth email password

---
+++ src/utils/components/InputForm.utils.tsx
@@ -0,0 +1,39 @@
+import { TextInput, Text } from "react-native-paper";
+import { colors } from "../colors.utils";
+import { commonStyles } from "../styles.utils";
+import { KeyboardTypeOptions } from "react-native";
+
+interface InputFormProps {
+  value: any;
+  setter: any;
+  label: any;
+  secure?: boolean;
+  left?: React.ReactNode;
+  right?: React.ReactNode;
+  type?: KeyboardTypeOptions;
+}
+
+export const InputForm = ({
+  value,
+  setter,
+  label,
+  secure = false,
+  left = undefined,
+  right = undefined,
+  type = "default",
+}: InputFormProps) => {
+  return (
+    <TextInput
+      keyboardType={type}
+      autoCapitalize="none"
+      secureTextEntry={secure}
+      label={<Text style={{ ...commonStyles.primaryText }}>{label}</Text>}
+      mode="outlined"
+      value={value}
+      textColor={colors.black}
+      onChangeText={(text) => setter(text)}
+      left={left}
+      right={right}
+    />
+  );
+};
---
+++ src/utils/styles.utils.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+import { colors } from "./colors.utils";
+
+export const commonStyles = StyleSheet.create({
+  viewWrapper: {
+    display: "flex",
+    width: "90%",
+    alignSelf: "center",
+    // backgroundColor: '#f02',
+  },
+  primaryText: {
+    color: colors.black,
+    fontFamily: "Inter",
+  },
+  secondaryText: {
+    color: colors.gray3,
+    fontFamily: "Inter-Medium",
+  },
+});
